fbe2e5cbe14fb81573eba9816c5d394eaf13a535
JethroNatividad <natividad.jet@gmail.com>
2024-01-03 21:09:34 +0800

    write problem

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+// Write a program to calculate the compound interest.
+// Inputs: starting amount, the number of years to invest, the interest rate, and the number of periods per year to compound.
+// Process: principal_amount * pow(1 + (interest_rate/times_compounded_per_year), times_compounded_per_year * invested_years)
+// Output: $1500 invested at 4.3% for 6 years compounded 4 times per year is $1938.84.
+
+fn calculate_compound_interest
+
 fn main() {
     println!("Hello, world!");
 }
