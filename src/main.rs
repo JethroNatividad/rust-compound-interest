4eb34ea6cfc7f51ffa21738fef048818b30b3486
JethroNatividad <natividad.jet@gmail.com>
2024-01-03 23:43:38 +0800

    write fb

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@
 // Process: principal_amount * pow(1 + (interest_rate/times_compounded_per_year), times_compounded_per_year * invested_years)
 // Output: $1500 invested at 4.3% for 6 years compounded 4 times per year is $1938.84.
 
-fn calculate_compound_interest
+fn calculate_compound_interest() -> f64 {
+    
+}
 
 fn main() {
     println!("Hello, world!");
